5cb7e1884715bd6f37e9df5f5da29c92a9109e46
ZiggySaidGrub <grubsaidgrub@gmail.com>
2024-11-29 22:11:14 -0700

    website or smthn

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const ansDiv = document.getElementById("answer");
 
 button.addEventListener("click", function() {
   // Code to execute when the button is clicked
-  let distance = document.getElementById("madelines").value;
+  let oldObj = document.getElementById("ansObj");
+  if(oldObj != null){oldObj.remove();}
+  let distance = document.getElementById("distance").value;
   answer = distance/3.81;
   ansObj = document.createElement("p");
-  ansObj.textContent = toString(answer)+" Squished Madelines Tall";
+  ansObj.id = "ansObj"
+  //ansObj.textContent = toString(answer)+" Squished Madelines Tall";
+  ansObj.textContent = "≈ "+answer+" Squished Madelines";
   ansDiv.appendChild(ansObj);
 });
