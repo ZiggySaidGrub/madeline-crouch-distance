79d5e0eeb5cb7da4dd5d0fd8fb3be677e73626eb
ZiggySaidGrub <grubsaidgrub@gmail.com>
2024-11-29 21:35:06 -0700

    test

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+  // Code to execute when the button is clicked
+  console.log("Button clicked!");
+});
