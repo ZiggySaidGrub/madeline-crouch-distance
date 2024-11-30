1f070cfaed562287a83e6a6ee9b861e4405fdafa
ZiggySaidGrub <grubsaidgrub@gmail.com>
2024-11-29 22:23:43 -0700

    New Input Method

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-const button = document.getElementById("button");
-const ansDiv = document.getElementById("answer");
+const ansObj = document.getElementById("ansObj");
 
-button.addEventListener("click", function() {
-  // Code to execute when the button is clicked
-  let oldObj = document.getElementById("ansObj");
-  if(oldObj != null){oldObj.remove();}
-  let distance = document.getElementById("distance").value;
-  answer = distance/3.81;
-  ansObj = document.createElement("p");
-  ansObj.id = "ansObj"
-  //ansObj.textContent = toString(answer)+" Squished Madelines Tall";
-  ansObj.textContent = "≈ "+answer+" Squished Madelines";
-  ansDiv.appendChild(ansObj);
-});
+function update() {
+    let distance = document.getElementById("distance").value;
+    answer = distance/3.81;
+    ansObj.textContent = "≈ "+answer+" Squished Madelines";
+    requestAnimationFrame(update);
+  }
+requestAnimationFrame(update);
