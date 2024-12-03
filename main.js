7d56d5effc06891bc8b3d9e7f5ba74effc18165b
ZiggySaidGrub <grubsaidgrub@gmail.com>
2024-12-03 11:19:23 -0700

    added goofy unit

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function roundToThousandth(num) {
 function update() {
     let distance = document.getElementById("distance").value;
     let unit = document.getElementById("unit").value;
-    let distances = [distance,distance/12,distance*3.280839895,distance*3280.839895,distance*0.032808398950131,distance*5280];
+    let distances = [distance,distance/12,distance*3.280839895,distance*3280.839895,distance*0.032808398950131,distance*5280,distance*(3.104e+16
+    ),distance*6076.11549];
     answer = distances[unit]/((6/11)*7);
     ansObj.textContent = "≈ "+roundToThousandth(answer)+" Squished Madelines";
     requestAnimationFrame(update);
