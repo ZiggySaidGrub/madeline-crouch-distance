623ddbb0fd1d9a33c3b88f346f90b57027f0ef29
ZiggySaidGrub <grubsaidgrub@gmail.com>
2024-12-02 18:02:03 -0700

    i forgot about miles lol

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function roundToThousandth(num) {
 function update() {
     let distance = document.getElementById("distance").value;
     let unit = document.getElementById("unit").value;
-    let distances = [distance,distance/12,distance*3.280839895,distance*3280.839895,distance*0.032808398950131];
+    let distances = [distance,distance/12,distance*3.280839895,distance*3280.839895,distance*0.032808398950131,distance*5280];
     answer = distances[unit]/((6/11)*7);
     ansObj.textContent = "≈ "+roundToThousandth(answer)+" Squished Madelines";
     requestAnimationFrame(update);
